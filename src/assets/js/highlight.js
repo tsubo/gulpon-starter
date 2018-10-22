609e60f2a81418a4fc6a5639532267d6f36a45d5
tsubo <ktsubota@gmail.com>
2018-10-23 00:16:14 +0900

    add xml highlight

--- src/assets/js/highlight.js
+++ src/assets/js/highlight.js
@@ -1,11 +1,13 @@
 import hljs from 'highlight.js/lib/highlight'
 
 import css from 'highlight.js/lib/languages/css'
+import xml from 'highlight.js/lib/languages/xml'
 import javascript from 'highlight.js/lib/languages/javascript'
 import php from 'highlight.js/lib/languages/php'
 import markdown from 'highlight.js/lib/languages/markdown'
 
 hljs.registerLanguage('css', css)
+hljs.registerLanguage('xml', xml)
 hljs.registerLanguage('javascript', javascript)
 hljs.registerLanguage('php', php)
 hljs.registerLanguage('markdown', markdown)
